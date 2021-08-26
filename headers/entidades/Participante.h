0a52a36d5b3541fa01af4537dd1c073b06d20a1f
Maria Claudia <mariaclaudia8.0@hotmail.com>
2021-08-26 13:36:17 -0300

    Mudanças nas entidades: inclusão de todos os dominios e definição de seus setters e getters. Alteração no projeto do code blocks. Inclusao de documentação doxygen nas entidades

--- headers/entidades/Participante.h
+++ headers/entidades/Participante.h
@@ -7,6 +7,11 @@
 #include <iostream>
 
 #include "../dominios/Matricula.h"
+#include "../dominios/Nome.h"
+#include "../dominios/Email.h"
+#include "../dominios/Telefone.h"
+#include "../dominios/Senha.h"
+#include "../dominios/Cargo.h"
 
 using namespace std;
 
@@ -15,16 +20,28 @@ class Participante
 {
 private:
     Matricula matricula;
-    string nome;
-    string sobrenome;
-    string email;
-    string telefone;
-    string senha;
-    string cargo;
+    Nome nome;
+    //Nome sobrenome;
+    Email email;
+    Telefone telefone;
+    Senha senha;
+    Cargo cargo;
 
 public:
     void setMatricula(const Matricula &);
     Matricula getMatricula() const;
+    void setNome(const Nome &);
+    Nome getNome() const;
+    //void setSobrenome(const Nome &);
+    //Nome getSobrenome() const;
+    void setEmail(const Email &);
+    Email getEmail() const;
+    void setTelefone(const Telefone &);
+    Telefone getTelefone() const;
+    void setSenha(const Senha &);
+    Senha getSenha() const;
+    void setCargo(const Cargo &);
+    Cargo getCargo() const;
 };
 
 #endif //PARTICIPANTE_H
--- src/entidades/Sessao.cpp
+++ src/entidades/Sessao.cpp
@@ -1,18 +1,43 @@
 #include "../../headers/entidades/Sessao.h"
 
+/// \brief Armazena o identificador da sessao em um objeto Codigo
+/// @param Codigo codigo
 void Sessao::setCodigo(const Codigo &codigo)
 {
     this->identificador = codigo;
 };
+
+/// \brief Retorna o identificador da sessao
+/// @param Codigo identificador
 Codigo Sessao::getCodigo() const
 {
     return identificador;
 };
+
+/// \brief Armazena a data da sessao em um objeto Data
+/// @param Data data
 void Sessao::setData(const Data &data)
 {
     this->data = data;
 };
+
+/// \brief Retorna a data da sessao
+/// @param Data data
 Data Sessao::getData() const
 {
     return data;
+};
+
+/// \brief Armazena o horario da sessao em um objeto Horario
+/// @param Horario horario
+void Sessao::setHorario(const Horario &horario)
+{
+    this->horario = horario;
+};
+
+/// \brief Retorna a horario da sessao
+/// @param Horario horario
+Horario Sessao::getHorario() const
+{
+    return horario;
 };
